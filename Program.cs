e5d2d8991e5ce3a420cebc44e863df2f8f9f58b8
cmptbeck <T.Beck@2019.ljmu.ac.uk>
2022-12-04 23:52:03 +0000

    Test Data Generation Added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SortingAlgorithmsBenchmark
+{
+    internal class Benchmark
+    {
+        static void Main(string[] args)
+        {
+            string[] benchmarkNames = 
+            {
+                "Quick sort     ",
+                "Merge sort     ",
+                "Bubble sort    ",
+                "Insertion sort ",
+                "Selection sort "
+            };
+
+            //Randomly generate an array of 1000 numbers.
+            int numbers = 1000;
+            int[] testData = generateArrayOfLength(numbers);
+
+            //Quick sort benchmark.
+
+            //Merge sort benchmark.
+
+            //Bubble sort benchmark.
+
+            //Insertion sort benchmark.
+
+            //Selection sort benchmark.
+
+            //Output Benchmark Results.
+            printResults(testData);
+        }
+
+        static int[] generateArrayOfLength(int length)
+        {
+            int[] array = new int[length];
+            Random rnd = new Random();
+
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = rnd.Next();
+            }
+
+            return array;
+        }
+
+        //Code used for testing program.
+        //Can be removed when program is finished.
+        static void printResults(int[] data) 
+        {
+            Console.WriteLine("[{0}]", string.Join(", ", data));
+        }
+    }
+}
